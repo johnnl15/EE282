79e8afbf19ca0f031de9e1c5c6f915b85e02c74e
johnnl15 <71590737+johnnl15@users.noreply.github.com>
2020-11-09 23:47:27 -0800

    Update hw3_annotation_summary.sh

--- hw3_annotation_summary.sh
+++ hw3_annotation_summary.sh
@@ -1,4 +1,9 @@
-wget "ftp://ftp.flybase.net/genomes//Drosophila_melanogaster/dmel_r6.36_FB2020_05/fasta/dmel-all-chromosome-r6.36.fasta.gz"" -O dmel-all-chromosome-r6.36.fasta.gz
-wget "ftp://ftp.flybase.net/genomes//Drosophila_melanogaster/dmel_r6.36_FB2020_05/fasta/md5sum.txt" -O md5sum.txt
+wget "ftp://ftp.flybase.net/genomes//Drosophila_melanogaster/dmel_r6.36_FB2020_05/gtf/dmel-all-r6.36.gtf.gz"
+wget "ftp://ftp.flybase.net/genomes//Drosophila_melanogaster/dmel_r6.36_FB2020_05/gtf/md5sum.txt"
 md5sum --check md5sum.txt
-faSize dmel-all-chromosome-r6.36.fasta.gz
+gunzip dmel-all-r6.36.gtf.gz
+
+cut -f3 'dmel-all-r6.36.gtf.gz'\
+|sort\
+|uniq -c\
+|sort -nr
